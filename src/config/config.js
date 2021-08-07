ba59ed8fdf7434d8c21fdd5af7cf71fab8b2eb82
MrNithi <bossrocker777@gmail.com>
2021-08-07 17:50:52 +0700

    Add config file

---
+++ src/config/config.js
@@ -0,0 +1,9 @@
+const dotenv = require("dotenv");
+const path = require("path");
+
+dotenv.config({ path: path.join(__dirname, "../../.env") });
+
+module.exports = {
+  env: process.env.NODE_ENV,
+  port: process.env.PORT,
+};
