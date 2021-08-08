fa864d142fc63110b944513b6e2c7fdd80ca09f9
MrNithi <bossrocker777@gmail.com>
2021-08-08 21:51:56 +0700

    Add user service

---
+++ src/services/user.service.js
@@ -0,0 +1,51 @@
+const httpStatus = require("http-status");
+const { User } = require("../models");
+const ApiError = require('../utils/ApiError');
+
+const createUser = async (userBody) => {
+  if (await User.isUsernameTaken(userBody.username)) {
+    throw new ApiError(httpStatus.BAD_REQUEST, "Username already taken");
+  }
+  return User.create(userBody);
+};
+
+const getUserById = async (id) => {
+  return User.findById(id);
+};
+
+const getUserByUsername = async (username) => {
+  return User.findOne({ username });
+};
+
+const updateUserById = async (userId, updateBody) => {
+  const user = await getUserById(userId);
+  if (!user) {
+    throw new ApiError(httpStatus.NOT_FOUND, "User not found");
+  }
+  if (
+    updateBody.username &&
+    (await User.isUsernameTaken(updateBody.username, userId))
+  ) {
+    throw new ApiError(httpStatus.BAD_REQUEST, "Username already taken");
+  }
+  Object.assign(user, updateBody);
+  await user.save();
+  return user;
+};
+
+const deleteUserById = async (userId) => {
+  const user = await getUserById(userId);
+  if (!user) {
+    throw new ApiError(httpStatus.NOT_FOUND, "User not found");
+  }
+  await user.remove();
+  return user;
+};
+
+module.exports = {
+  createUser,
+  getUserById,
+  getUserByUsername,
+  updateUserById,
+  deleteUserById,
+};
