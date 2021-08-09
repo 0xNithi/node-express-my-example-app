82736a2ded76dba619f1684bf3d26521ac9b9efe
MrNithi <bossrocker777@gmail.com>
2021-08-09 16:06:38 +0700

    Add JWT config

--- src/config/config.js
+++ src/config/config.js
@@ -6,4 +6,9 @@ dotenv.config({ path: path.join(__dirname, "../../.env") });
 module.exports = {
   env: process.env.NODE_ENV,
   port: process.env.PORT,
+  jwt: {
+    secret: process.env.JWT_SECRET,
+    accessExpirationMinutes: process.env.JWT_ACCESS_EXPIRATION_MINUTES,
+    refreshExpirationDays: process.env.JWT_REFRESH_EXPIRATION_DAYS,
+  },
 };
