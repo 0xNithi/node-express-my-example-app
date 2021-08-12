0ea20680c02619b73bf890f9bc73351444d45879
MrNithi <bossrocker777@gmail.com>
2021-08-12 20:40:59 +0700

    Add mongoose config

--- src/config/config.js
+++ src/config/config.js
@@ -6,6 +6,14 @@ dotenv.config({ path: path.join(__dirname, '../../.env') });
 module.exports = {
   env: process.env.NODE_ENV,
   port: process.env.PORT,
+  mongoose: {
+    url: process.env.MONGODB_URL,
+    options: {
+      useCreateIndex: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    },
+  },
   jwt: {
     secret: process.env.NODE_ENV === 'production' ? process.env.JWT_SECRET : 'secret',
     accessExpirationMinutes: process.env.JWT_ACCESS_EXPIRATION_MINUTES,
