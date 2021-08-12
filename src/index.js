f0726a311bca28d94986176b0336c27175b7a16e
MrNithi <bossrocker777@gmail.com>
2021-08-12 21:41:04 +0700

    Connect to mongodb

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
+const mongoose = require('mongoose');
 const app = require('./app');
 const config = require('./config/config');
+const logger = require('./config/logger');
 
-app.listen(config.port, () => {
-  console.log(`Example app listening at http://localhost:${config.port}`);
+mongoose.connect(config.mongoose.url, config.mongoose.options).then(() => {
+  logger.info('Connected to MongoDB');
+  app.listen(config.port, () => {
+    logger.info(`Listening to port ${config.port}`);
+  });
 });
