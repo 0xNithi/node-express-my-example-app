fc721ea9176cd9a5118900dfe4dd193fec7533b2
MrNithi <bossrocker777@gmail.com>
2021-08-12 21:40:23 +0700

    Add route to app

--- src/app.js
+++ src/app.js
@@ -1,11 +1,37 @@
 const express = require('express');
+const cors = require('cors');
+const passport = require('passport');
+const httpStatus = require('http-status');
+const { jwtStrategy } = require('./config/passport');
+const routes = require('./routes/v1');
+const { errorConverter, errorHandler } = require('./middlewares/error');
+const ApiError = require('./utils/ApiError');
+
 const app = express();
 
 // parse json request body
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+// enable cors
+app.use(cors());
+app.options('*', cors());
+
+// jwt authentication
+app.use(passport.initialize());
+passport.use('jwt', jwtStrategy);
+
+// v1 api routes
+app.use('/v1', routes);
+
+// send back a 404 error for any unknown api request
+app.use((req, res, next) => {
+  next(new ApiError(httpStatus.NOT_FOUND, 'Not found'));
 });
+
+// convert error to ApiError, if needed
+app.use(errorConverter);
+
+// handle error
+app.use(errorHandler);
 
 module.exports = app;
