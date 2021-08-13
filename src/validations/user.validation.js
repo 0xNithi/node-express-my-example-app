54054fe59979e1694e46997b2093b6b66d038e8a
MrNithi <bossrocker777@gmail.com>
2021-08-13 10:27:05 +0700

    Add user validation

---
+++ src/validations/user.validation.js
@@ -0,0 +1,32 @@
+const Joi = require('joi');
+const { password, objectId } = require('./custom.validation');
+
+const getUser = {
+  params: Joi.object().keys({
+    userId: Joi.string().custom(objectId),
+  }),
+};
+
+const updateUser = {
+  params: Joi.object().keys({
+    userId: Joi.required().custom(objectId),
+  }),
+  body: Joi.object()
+    .keys({
+      username: Joi.string(),
+      password: Joi.string().custom(password),
+    })
+    .min(1),
+};
+
+const deleteUser = {
+  params: Joi.object().keys({
+    userId: Joi.string().custom(objectId),
+  }),
+};
+
+module.exports = {
+  getUser,
+  updateUser,
+  deleteUser,
+};
