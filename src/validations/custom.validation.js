a8775736264045d37076ef1e33f05c04128bfdf9
MrNithi <bossrocker777@gmail.com>
2021-08-13 10:25:18 +0700

    Add objectId to custom validation

--- src/validations/custom.validation.js
+++ src/validations/custom.validation.js
@@ -1,3 +1,10 @@
+const objectId = (value, helpers) => {
+  if (!value.match(/^[0-9a-fA-F]{24}$/)) {
+    return helpers.message('"{{#label}}" must be a valid mongo id');
+  }
+  return value;
+};
+
 const password = (value, helpers) => {
   if (value.length < 8) {
     return helpers.message('password must be at least 8 characters');
@@ -9,5 +16,6 @@ const password = (value, helpers) => {
 };
 
 module.exports = {
+  objectId,
   password,
 };
