11dc56ab04b2c5e6acaf841f2bd78a1e6bfda02d
MrNithi <bossrocker777@gmail.com>
2021-08-14 21:31:13 +0700

    Remove PORT in dotenv

--- src/config/config.js
+++ src/config/config.js
@@ -5,7 +5,7 @@ dotenv.config({ path: path.join(__dirname, '../../.env') });
 
 module.exports = {
   env: process.env.NODE_ENV,
-  port: process.env.PORT,
+  port: process.env.PORT || 3000,
   mongoose: {
     url: process.env.MONGODB_URL,
     options: {
