7eb65ffe2e8a7806d04e783cbe201690b44914e6
MrNithi <bossrocker777@gmail.com>
2021-08-14 18:52:32 +0700

    Add article controller

---
+++ src/controllers/article.controller.js
@@ -0,0 +1,43 @@
+const httpStatus = require('http-status');
+const pick = require('../utils/pick');
+const ApiError = require('../utils/ApiError');
+const catchAsync = require('../utils/catchAsync');
+const { articleService } = require('../services');
+
+const createArticle = catchAsync(async (req, res) => {
+  const article = await articleService.createArticle(req.body);
+  res.status(httpStatus.CREATED).send(article);
+});
+
+const getArticles = catchAsync(async (req, res) => {
+  const filter = pick(req.query, ['title', 'body']);
+  const options = pick(req.query, ['sortBy', 'limit', 'page']);
+  const result = await articleService.queryArticle(filter, options);
+  res.send(result);
+});
+
+const getArticle = catchAsync(async (req, res) => {
+  const article = await articleService.getArticleById(req.params.articleId);
+  if (!article) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'Article not found');
+  }
+  res.send(article);
+});
+
+const updateArticle = catchAsync(async (req, res) => {
+  const article = await articleService.updateArticleById(req.params.articleId, req.body);
+  res.send(article);
+});
+
+const deleteArticle = catchAsync(async (req, res) => {
+  await articleService.deleteArticleById(req.params.articleId);
+  res.status(httpStatus.NO_CONTENT).send();
+});
+
+module.exports = {
+  createArticle,
+  getArticles,
+  getArticle,
+  updateArticle,
+  deleteArticle,
+};
