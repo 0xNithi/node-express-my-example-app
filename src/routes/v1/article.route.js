91f81dbe696435dae5f8ac36957dc5ab8b9e968d
MrNithi <bossrocker777@gmail.com>
2021-08-14 19:03:45 +0700

    Remove auth when get data

--- src/routes/v1/article.route.js
+++ src/routes/v1/article.route.js
@@ -9,7 +9,7 @@ const router = express.Router();
 router
   .route('/')
   .post(auth, validate(articleValidation.createArticle), articleController.createArticle)
-  .get(auth, validate(articleValidation.getArticles), articleController.getArticles);
+  .get(validate(articleValidation.getArticles), articleController.getArticles);
 
 router
   .route('/:articleId')
