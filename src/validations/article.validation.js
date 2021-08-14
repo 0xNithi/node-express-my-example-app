c486ce0c3f9360113bdf3d0c680d9d50cc014039
MrNithi <bossrocker777@gmail.com>
2021-08-14 18:50:03 +0700

    Add article validate

---
+++ src/validations/article.validation.js
@@ -0,0 +1,51 @@
+const Joi = require('joi');
+const { objectId } = require('./custom.validation');
+
+const createArticle = {
+  body: Joi.object().keys({
+    title: Joi.string().required(),
+    body: Joi.string().required(),
+  }),
+};
+
+const getArticles = {
+  query: Joi.object().keys({
+    title: Joi.string(),
+    body: Joi.string(),
+    sortBy: Joi.string(),
+    limit: Joi.number().integer(),
+    page: Joi.number().integer(),
+  }),
+};
+
+const getArticle = {
+  params: Joi.object().keys({
+    articleId: Joi.string().custom(objectId),
+  }),
+};
+
+const updateArticle = {
+  params: Joi.object().keys({
+    articleId: Joi.required().custom(objectId),
+  }),
+  body: Joi.object()
+    .keys({
+      title: Joi.string(),
+      body: Joi.string(),
+    })
+    .min(1),
+};
+
+const deleteArticle = {
+  params: Joi.object().keys({
+    articleId: Joi.string().custom(objectId),
+  }),
+};
+
+module.exports = {
+  createArticle,
+  getArticles,
+  getArticle,
+  updateArticle,
+  deleteArticle,
+};
--- src/validations/index.js
+++ src/validations/index.js
@@ -1,1 +1,3 @@
 module.exports.authValidation = require('./auth.validation');
+module.exports.userValidation = require('./user.validation');
+module.exports.articleValidation = require('./article.validation');
