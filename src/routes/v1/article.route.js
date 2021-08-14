7fa701dd1259e9bdf2562b009b7392530e560567
MrNithi <bossrocker777@gmail.com>
2021-08-14 18:52:51 +0700

    Add article route

---
+++ src/routes/v1/article.route.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const auth = require('../../middlewares/auth');
+const validate = require('../../middlewares/validate');
+const articleValidation = require('../../validations/article.validation');
+const articleController = require('../../controllers/article.controller');
+
+const router = express.Router();
+
+router
+  .route('/')
+  .post(auth, validate(articleValidation.createArticle), articleController.createArticle)
+  .get(auth, validate(articleValidation.getArticles), articleController.getArticles);
+
+router
+  .route('/:articleId')
+  .get(validate(articleValidation.getArticle), articleController.getArticle)
+  .patch(auth, validate(articleValidation.updateArticle), articleController.updateArticle)
+  .delete(auth, validate(articleValidation.deleteArticle), articleController.deleteArticle);
+
+module.exports = router;
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const authRoute = require('./auth.route');
 const userRoute = require('./user.route');
+const articleRoute = require('./article.route');
 
 const router = express.Router();
 
@@ -12,6 +13,10 @@ const defaultRoutes = [
   {
     path: '/users',
     route: userRoute,
+  },
+  {
+    path: '/articles',
+    route: articleRoute,
   },
 ];
 
